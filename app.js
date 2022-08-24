c5935f5bb05afe541e0bb64b5c4c66fd3cf32262
SamuelLin213 <samuelddlin@gmail.com>
2022-08-23 22:08:25 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+const handleClick = (event) => {
+    document.querySelector(".active").classList.remove("active");
+    document.getElementById(event).classList.add("active");
+}
+
+function filterSelection(c) {
+    var x;
+    x = document.querySelectorAll(".portfolio-item");
+    if (c == "all") 
+        c = "";
+    Array.from(x).forEach(item => {
+        removeActiveClass(item, "show");
+        if (item.className.indexOf(c) > -1)
+            addActiveClass(item, "show");
+    });
+};
+function addActiveClass(ele, name) {
+    var i, arr1, arr2;
+    arr1 = ele.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+       if (arr1.indexOf(arr2[i]) == -1) {
+          ele.className += " " + arr2[i];
+       }
+    }
+};
+function removeActiveClass(ele, name) {
+    var i, arr1, arr2;
+    arr1 = ele.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+       while (arr1.indexOf(arr2[i]) > -1) {
+          arr1.splice(arr1.indexOf(arr2[i]), 1);
+       }
+    }
+    ele.className = arr1.join(" ");
+};
+
+var btns = document.querySelector(".filters").getElementsByTagName("button");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("activeFilter");
+        current[0].className = current[0].className.replace(" activeFilter", "");
+        this.className += " activeFilter";
+    });
+}
+
+filterSelection("all");
